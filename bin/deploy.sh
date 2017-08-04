aff8ce9af15cc4f629dccaf789d13f7eb5a51873
Karel Maesen <karel@geovise.com>
2017-08-04 16:24:58 +0200

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -o nounset
 set -o errexit
- 
+set -x
+set -e
+
 NFLAG=""
- 
+
 while getopts ":n" opt; do
   case $opt in
     n)
@@ -14,13 +16,14 @@ while getopts ":n" opt; do
       ;;
   esac
 done
- 
+
 # Set the environment by loading from the file "environment" in the same directory
 DIR="$( cd "$( dirname $( dirname "$0" ) )" && pwd)"
+echo "$DIR"
 source "$DIR/.env"
- 
+cat "$DIR/.env"
 echo "Deploying ${DIR}/${DEPLOY_SOURCE_DIR} to ${DEPLOY_ACCOUNT}@${DEPLOY_SERVER}:${DEPLOY_DEST_DIR}"
- 
-docpad generate --env static
+
+#docpad generate --env static
 chmod -R og+Xr out
-rsync $NFLAG -rvzp --size-only --delete --exclude-from="$DIR/.deployignore" "${DIR}/${DEPLOY_SOURCE_DIR}" "${DEPLOY_ACCOUNT}@${DEPLOY_SERVER}:${DEPLOY_DEST_DIR}"
+rsync $NFLAG -e "$SSH" -rvzp --size-only --delete --exclude-from="$DIR/.deployignore" "${DIR}/${DEPLOY_SOURCE_DIR}" "${DEPLOY_ACCOUNT}@${DEPLOY_SERVER}:${DEPLOY_DEST_DIR}"
